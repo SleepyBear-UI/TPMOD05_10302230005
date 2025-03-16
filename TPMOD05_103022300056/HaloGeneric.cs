c59c44b982bd26e9e6c07d05c9b20ca64ce73f65
SleepyBear-UI <daffamantap29@gmail.com>
2025-03-17 04:58:36 +0700

    menambahkan class HaloGeneric dengan method sapauser

---
+++ TPMOD05_103022300056/HaloGeneric.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace TPMOD05_103022300056
+{
+	public class HaloGeneric
+	{
+		// Method sapa user yang dimana dia akan mengembalikan Output
+		// berupa " Halo User " + User yang berasal dari parameter Sapa User
+		public void SapaUser(String User)
+		{
+			Console.WriteLine("Halo User " + User);
+		}
+	}
+}
+
--- TPMOD05_103022300056/Program.cs
+++ TPMOD05_103022300056/Program.cs
@@ -1,3 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
 
+using TPMOD05_103022300056;
+
+// Membuat Object Halo agar dapat menggunakan
+// Funsi Sapa User yang berada di HaloGeneric.
+HaloGeneric halo = new HaloGeneric();
+
+// Input Nama user.
+String User = Console.ReadLine();
+
+// memanggil fungsi sapa user.
+halo.SapaUser(User);
