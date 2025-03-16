97a83c1eabb0a0d094ce85f2e3bfef9763ee20b6
SleepyBear-UI <daffamantap29@gmail.com>
2025-03-17 05:14:08 +0700

    menambah class data generic

--- TPMOD05_103022300056/Program.cs
+++ TPMOD05_103022300056/Program.cs
@@ -1,3 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using TPMOD05_103022300056;
 
+// Membuat objek DataGeneric dengan tipe string kemudian mengisi Data dengan Pribadi
+DataGeneric<string> dataNIM = new DataGeneric<string>("103022300056");
+
+// Memanggil method PrintData untuk mencetak Data yang berada di DataGeneric
+dataNIM.PrintData();
